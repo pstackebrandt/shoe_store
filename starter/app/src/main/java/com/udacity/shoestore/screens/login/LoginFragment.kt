235f06f4d04138217492eb151b4aacb22c07cf70
Peter Stackebrandt <peter.stackebrandt@immowelt.de>
2021-02-28 16:36:46 +0100

    Add Bind to view model in login fragment

--- starter/app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -1,12 +1,16 @@
 package com.udacity.shoestore.screens.login
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.udacity.shoestore.R
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment.findNavController
 
 /**
  * Login page. Demo. Login with email and password.
@@ -14,15 +18,33 @@ import com.udacity.shoestore.R
  */
 class LoginFragment : Fragment() {
 
+    private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: FragmentLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-            }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+       //return inflater.inflate(R.layout.fragment_login, container, false)
+
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_login,
+            container,
+            false
+        )
+
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        binding.loginViewModel = viewModel
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
 }
